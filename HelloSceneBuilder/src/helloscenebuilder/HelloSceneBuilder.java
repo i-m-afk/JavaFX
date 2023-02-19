00db7542679685fc909a323153c569374815a251
i-m-afk <rishavkumar208@gmail.com>
2023-02-19 20:28:43 +0530

    removed unused imports

--- HelloSceneBuilder/src/helloscenebuilder/HelloSceneBuilder.java
+++ HelloSceneBuilder/src/helloscenebuilder/HelloSceneBuilder.java
@@ -5,9 +5,7 @@
 package helloscenebuilder;
 
 import javafx.application.Application;
-import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Group;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
